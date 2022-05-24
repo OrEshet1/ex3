ced6b8aea1798e7c91ecf74eaec574c6d66af908
PolinaShihel <polinashihel@campus.technion.ac.il>
2022-05-24 23:57:56 +0300

    finished implementing HealthPoints (not tested, need to figure out how to fix + operator - no access to provate fields)

--- HealthPoints.cpp
+++ HealthPoints.cpp
@@ -1,7 +1,7 @@
 #include "HealthPoints.h"
 
 HealthPoints::HealthPoints(const int maxHealth):
-m_healthPoints(maxHealth)
+m_healthPoints(maxHealth), m_maxHealthPoints(maxHealth)
 {
     if(maxHealth <= 0)
     {
@@ -9,7 +9,81 @@ m_healthPoints(maxHealth)
     }
 }
 
-HealthPoints &HealthPoints::operator+(const int value) const
+HealthPoints& HealthPoints::operator+=(const int points)
 {
-    return this->m_healthPoints + value;
+    int maxHealth = this->m_maxHealthPoints;
+    int currentPoints = this->m_healthPoints;
+    if((currentPoints + points)>maxHealth)
+    {
+        this->m_healthPoints = maxHealth;
+        return *this;
+    }
+    if((currentPoints + points) < 0)
+    {
+        this->m_healthPoints = 0;
+        return *this;
+    }
+    this->m_healthPoints -= points;
+    return *this;
+}
+
+HealthPoints operator+(const HealthPoints &current, const HealthPoints &other)
+{
+    HealthPoints result = current;
+    result += other;
+    return result;
+}
+
+HealthPoints& HealthPoints::operator-=(const int points)
+{
+    int maxHealth = this->m_maxHealthPoints;
+    int currentPoints = this->m_healthPoints;
+    if((currentPoints - points)>maxHealth)
+    {
+        this->m_healthPoints = maxHealth;
+        return *this;
+    }
+    if(currentPoints - points < 0)
+    {
+        this->m_healthPoints = 0;
+        return *this;
+    }
+    this->m_healthPoints -= points;
+    return *this;
+}
+
+HealthPoints operator-(const HealthPoints& current, const int points)
+{
+    HealthPoints result = current;
+    result-=points;
+    return result;
+}
+
+bool operator==(const HealthPoints& current, const HealthPoints& other)
+{
+    return (current.m_healthPoints == other.m_healthPoints);
+}
+
+bool operator<(const HealthPoints& current, const HealthPoints& other)
+{
+    return (current.m_healthPoints < other.m_healthPoints);
+}
+
+bool operator>(const HealthPoints& current, const HealthPoints& other)
+{
+    return ((!(current < other))&&(!(current == other)));
+}
+bool operator>=(const HealthPoints& current, const HealthPoints& other)
+{
+    return ((current > other)||(current == other));
+}
+
+bool operator<=(const HealthPoints& current, const HealthPoints& other)
+{
+    return ((current < other)||(current == other));
+}
+
+bool operator!=(const HealthPoints& current, const HealthPoints& other)
+{
+    return (!(current == other));
 }
--- HealthPoints.h
+++ HealthPoints.h
@@ -18,40 +18,32 @@ public:
     HealthPoints(const int maxHealth = DEFAULT_MAX_HEALTH);
 
     /*
-     * Addition operator of HealthPoints class
-     *
-     * @param value - The value that the HealthPoints is to be increased by
-     * @return -
-     *      A reference to the HealthPoints object
-     */
-    HealthPoints& operator+(const int value) const;
-
-    /*
-     * Subtraction operator of HealthPoints class
-     *
-     * @param value - The value that the HealthPoints is to be decreased by
-     * @return -
-     *      A reference to the HealthPoints object
-     */
-    HealthPoints& operator-(const int value) const;
-
-    /*
      * Addition assignment operator of HealthPoints class
      *
-     * @param value - The value that the HealthPoints is to be increased by
+     * @param points - Number of points to be added to the current HealthPoints object
      * @return -
      *      A reference to the HealthPoints object
      */
-    HealthPoints& operator+=(const int value);
+    HealthPoints& operator+=(const int points);
 
     /*
      * Subtraction assignment operator of HealthPoints class
      *
-     * @param value - The value that the HealthPoints is to be decreased by
+     * @param points - Number of points to be decreased from the current HealthPoints object
      * @return -
      *      A reference to the HealthPoints object
      */
-    HealthPoints& operator-=(const int value);
+    HealthPoints& operator-=(const int points);
+
+    /*
+     * Subtraction operator of HealthPoints class
+     *
+     * @param current - Reference to the first HealthPoints object that's being subtracted from
+     * @param points - Number of points to be decreased from the current HealthPoints object
+     * @return -
+     *      The object after the subtraction
+     */
+    HealthPoints operator-(const HealthPoints& current, const int points);
 
     /*
      * Equality operator of HealthPoints class
@@ -64,16 +56,6 @@ public:
     friend bool operator==(const HealthPoints& current, const HealthPoints& other);
 
     /*
-     * Not-Equal operator of HealthPoints class
-     *
-     * @param current - Reference to the current HealthPoints object being compared
-     * @param other - Reference to HealthPoints object to be compared with
-     * @ return
-     *      True if objects are different, False if equal
-     */
-    friend bool operator!=(const HealthPoints& current, const HealthPoints& other);
-
-    /*
      * Lesser-than operator of HealthPoints class
      *
      * @param current - Reference to the current HealthPoints object being compared
@@ -83,39 +65,6 @@ public:
      *      False otherwise.
      */
     friend bool operator<(const HealthPoints& current, const HealthPoints& other);
-
-    /*
-     * Lesser-or-equal than operator of HealthPoints class
-     *
-     * @param current - Reference to the current HealthPoints object being compared
-     * @param other - Reference to HealthPoints object to be compared with
-     * @ return
-     *      True if the value of health of the current object is lesser than the other's,
-     *      or equal to it, False otherwise.
-     */
-    friend bool operator<=(const HealthPoints& current, const HealthPoints& other);
-
-    /*
-    * Greater-than operator of HealthPoints class
-    *
-    * @param current - Reference to the current HealthPoints object being compared
-    * @param other - Reference to HealthPoints object to be compared with
-    * @ return
-    *      True if the value of health of the current object is greater than the other's,
-    *      False otherwise.
-    */
-    friend bool operator>(const HealthPoints& current, const HealthPoints& other);
-
-    /*
-     * Greater-or-equal than operator of HealthPoints class
-     *
-     * @param current - Reference to the current HealthPoints object being compared
-     * @param other - Reference to HealthPoints object to be compared with
-     * @ return
-     *      True if the value of health of the current object is greater than the other's,
-     *      or equal to it, False otherwise.
-     */
-    friend bool operator>=(const HealthPoints& current, const HealthPoints& other);
 
     /*
      * Output operator of HealthPoints class
@@ -130,5 +79,60 @@ public:
     class InvalidArgument {};
 private:
     int m_healthPoints;
+    const int m_maxHealthPoints;
 };
+
+/*
+ * Greater-or-equal than operator of HealthPoints class
+ *
+ * @param current - Reference to the current HealthPoints object being compared
+ * @param other - Reference to HealthPoints object to be compared with
+ * @ return
+ *      True if the value of health of the current object is greater than the other's,
+ *      or equal to it, False otherwise.
+ */
+bool operator>=(const HealthPoints& current, const HealthPoints& other);
+
+/*
+ * Lesser-or-equal than operator of HealthPoints class
+ *
+ * @param current - Reference to the current HealthPoints object being compared
+ * @param other - Reference to HealthPoints object to be compared with
+ * @ return
+ *      True if the value of health of the current object is lesser than the other's,
+ *      or equal to it, False otherwise.
+ */
+bool operator<=(const HealthPoints& current, const HealthPoints& other);
+
+/*
+* Greater-than operator of HealthPoints class
+*
+* @param current - Reference to the current HealthPoints object being compared
+* @param other - Reference to HealthPoints object to be compared with
+* @ return
+*      True if the value of health of the current object is greater than the other's,
+*      False otherwise.
+*/
+bool operator>(const HealthPoints& current, const HealthPoints& other);
+
+/*
+ * Not-Equal operator of HealthPoints class
+ *
+ * @param current - Reference to the current HealthPoints object being compared
+ * @param other - Reference to HealthPoints object to be compared with
+ * @ return
+ *      True if objects are different, False if equal
+ */
+bool operator!=(const HealthPoints& current, const HealthPoints& other);
+
+/*
+  * Addition operator of HealthPoints class
+  *
+  * @param current - Reference to the first HealthPoints object that's being added
+  * @param other - Reference to second HealthPoints object to be added to the first
+  * @return -
+  *      The object with the sum of the received HealthPoints objects
+  */
+HealthPoints operator+(const HealthPoints& current, const HealthPoints& other );
+
 #endif //EX3_HEALTHPOINTS_H
