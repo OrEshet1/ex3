d45e7458c4666e4c29b50ca1b47e4ed0d7374b89
PolinaShihel <polinashihel@campus.technion.ac.il>
2022-05-31 13:03:29 +0300

    passed segel tests

--- Queue.h
+++ Queue.h
@@ -208,44 +208,44 @@ public:
 
     class EmptyQueue {};
 private:
-	int m_size;
-	template<class T>
-	class Element;
-	Element<T>* m_first;
-	Element<T>* m_last;
+    int m_size;
+    template<class T>
+    class Element;
+    Element<T>* m_first;
+    Element<T>* m_last;
 
 public:
-	class Iterator
-	{
-	public:
-		/*
-		* C'tor of Iterator Class
-		*/
-		Iterator(Queue<T>* queue, int index):
+    class Iterator
+    {
+    public:
+        /*
+        * C'tor of Iterator Class
+        */
+        Iterator(Queue<T>* queue, int index):
                 m_queue(queue), m_index(index)
         {
         }
 
-		/*
-		* Copy C'tor of Iterator class.
-		*
-		* @param source - reference to a Iterator object.
-		* @return
-		*      reference to a Iterator object.
-		*/
-		Iterator(const Iterator& source):
+        /*
+        * Copy C'tor of Iterator class.
+        *
+        * @param source - reference to a Iterator object.
+        * @return
+        *      reference to a Iterator object.
+        */
+        Iterator(const Iterator& source):
                 m_queue(source.m_queue), m_index(source.m_index)
         {
         }
 
-		/*
-		* Assignment operator of Iterator class.
-		*
-		* @param source - reference to an Iterator object.
-		* @return
-		*      reference to an Iterator object.
-		*/
-		Iterator& operator=(const Iterator& source)
+        /*
+        * Assignment operator of Iterator class.
+        *
+        * @param source - reference to an Iterator object.
+        * @return
+        *      reference to an Iterator object.
+        */
+        Iterator& operator=(const Iterator& source)
         {
             if(this == source)
             {
@@ -256,18 +256,18 @@ public:
             return *this;
         }
 
-		/*
-		* D'tor of Iterator Class
-		*/
-		~Iterator() = default;
+        /*
+        * D'tor of Iterator Class
+        */
+        ~Iterator() = default;
 
-		/*
-		* Dereference operator of Iterator class
-		*
-		* @return
-		*      Reference to the value of the current iteration's queue element
-		*/
-		T& operator*() const
+        /*
+        * Dereference operator of Iterator class
+        *
+        * @return
+        *      Reference to the value of the current iteration's queue element
+        */
+        T& operator*() const
         {
             if (m_index == m_queue->size()) {
                 throw InvalidOperation();
@@ -283,30 +283,30 @@ public:
             return currentElement->getData();
         }
 
-		/*
-		* Pre-increment operator of Iterator class
-		*
-		* @return
-		*      Reference to an iterator indicating the next element of the queue
-		*/
-		Iterator& operator++()
+        /*
+        * Pre-increment operator of Iterator class
+        *
+        * @return
+        *      Reference to an iterator indicating the next element of the queue
+        */
+        Iterator& operator++()
         {
-            if (m_index == m_queue.size()) {
+            if (m_index == m_queue->size()) {
                 throw InvalidOperation();
             }
             ++m_index;
             return *this;
         }
 
-		/*
-		* Post-increment operator of Iterator class
-		*
-		* @return
-		*      Reference to an iterator indicating the next element of the queue
-		*/
-		Iterator operator++(T)
+        /*
+        * Post-increment operator of Iterator class
+        *
+        * @return
+        *      Reference to an iterator indicating the next element of the queue
+        */
+        Iterator operator++(T)
         {
-            if (m_index == m_queue.size()) {
+            if (m_index == m_queue->size()) {
                 throw InvalidOperation();
             }
 
@@ -315,72 +315,72 @@ public:
             return result;
         }
 
-		/*
-		* Not-Equal operator of Iterator class
-		*
-		* @param other - Reference to Iterator object to be compared with
-		* @ return
-		*      True if objects are different, False if equal
-		*/
-		bool operator!=(const Iterator& other) const
+        /*
+        * Not-Equal operator of Iterator class
+        *
+        * @param other - Reference to Iterator object to be compared with
+        * @ return
+        *      True if objects are different, False if equal
+        */
+        bool operator!=(const Iterator& other) const
         {
             return this->m_index != other.m_index || this->m_queue != other.m_queue;
         }
 
-		/*Exception thrown when there's an attempt to affect an iterator
-		that has reached the end of its queue
-		class InvalidOperation : public std::logic_error
-		{
-		public:
+        /*Exception thrown when there's an attempt to affect an iterator
+        that has reached the end of its queue
+        class InvalidOperation : public std::logic_error
+        {
+        public:
 
-			* C'tor of InvalidOperation class
+            * C'tor of InvalidOperation class
 
-			InvalidOperation():
+            InvalidOperation():
                     std::logic_error("Iterator has reached the end of the queue")
             {
             }
 
-		};*/
+        };*/
         class InvalidOperation {};
 
-	private:
+    private:
         Queue<T>* m_queue;
-		int m_index;
-	};
+        int m_index;
+    };
 
-	class ConstIterator
-	{
-	public:
-		/*
-		* C'tor of ConstIterator Class
-		*/
-		ConstIterator(const Queue<T>* queue, int index):
-        m_queue(queue), m_index(index)
+    class ConstIterator
+    {
+    public:
+        /*
+        * C'tor of ConstIterator Class
+        */
+        ConstIterator(const Queue<T>* queue, int index):
+                m_queue(queue), m_index(index)
         {
         }
 
-		/*
-		* Copy C'tor of ConstIterator class.
-		*
-		* @param source - reference to a ConstIterator object.
-		* @return
-		*      reference to a ConstIterator object.
-		*/
-		ConstIterator(const ConstIterator& source) :
-        m_index(source.m_index)
+        /*
+        * Copy C'tor of ConstIterator class.
+        *
+        * @param source - reference to a ConstIterator object.
+        * @return
+        *      reference to a ConstIterator object.
+        */
+        ConstIterator(const ConstIterator& source) :
+                m_index(source.m_index)
         {
             const Queue<T>* constQueue = source.m_queue;
             m_queue = constQueue;
         }
 
-		/*
-		* Assignment operator of ConstIterator class.
-		*
-		* @param source - reference to a ConstIterator object.
-		* @return
-		*      reference to a ConstIterator object.
-		*/
-		ConstIterator& operator=(const ConstIterator& source)
+        /*
+        * Assignment operator of ConstIterator class.
+        *
+        * @param source - reference to a ConstIterator object.
+        * @return
+        *      reference to a ConstIterator object.
+        */
+        ConstIterator& operator=(const ConstIterator& source)
         {
             if(this == source)
             {
@@ -391,18 +391,18 @@ public:
             return *this;
         }
 
-		/*
-		* D'tor of Iterator Class
-		*/
-		~ConstIterator() = default;
+        /*
+        * D'tor of Iterator Class
+        */
+        ~ConstIterator() = default;
 
-		/*
-		* Dereference operator of ConstIterator class
-		*
-		* @return
-		*      Const reference to the value of the current iteration's queue element
-		*/
-		const T& operator*() const
+        /*
+        * Dereference operator of ConstIterator class
+        *
+        * @return
+        *      Const reference to the value of the current iteration's queue element
+        */
+        const T& operator*() const
         {
             if (m_index == m_queue->size()) {
                 throw InvalidOperation();
@@ -418,13 +418,13 @@ public:
             return currentElement->getData();
         }
 
-		/*
-		* Pre-increment operator of ConstIterator class
-		*
-		* @return
-		*      Reference to a ConstIterator indicating the next element of the queue
-		*/
-		ConstIterator& operator++()
+        /*
+        * Pre-increment operator of ConstIterator class
+        *
+        * @return
+        *      Reference to a ConstIterator indicating the next element of the queue
+        */
+        ConstIterator& operator++()
         {
             if (m_index == m_queue->size()) {
                 throw InvalidOperation();
@@ -433,13 +433,13 @@ public:
             return *this;
         }
 
-		/*
-		* Post-increment operator of ConstIterator class
-		*
-		* @return
-		*      Reference to a ConstIterator indicating the next element of the queue
-		*/
-		ConstIterator operator++(T)
+        /*
+        * Post-increment operator of ConstIterator class
+        *
+        * @return
+        *      Reference to a ConstIterator indicating the next element of the queue
+        */
+        ConstIterator operator++(T)
         {
             if (m_index == m_queue->size()) {
                 throw InvalidOperation();
@@ -450,50 +450,50 @@ public:
             return result;
         }
 
-		/*
-		* Equality operator of ConstIterator class
-		*
-		* @param other - Reference to ConstIterator object to be compared with
-		* @ return
-		*      True if objects are equal, False if different
-		*/
-		bool operator==(const ConstIterator& iterator) const
+        /*
+        * Equality operator of ConstIterator class
+        *
+        * @param other - Reference to ConstIterator object to be compared with
+        * @ return
+        *      True if objects are equal, False if different
+        */
+        bool operator==(const ConstIterator& iterator) const
         {
             return this->m_queue == iterator.m_queue && this->m_index == iterator.m_index;
         }
 
-		/*
-		* Not-Equal operator of Iterator class
-		*
-		* @param other - Reference to Iterator object to be compared with
-		* @ return
-		*      True if objects are different, False if equal
-		*/
-		bool operator!=(const ConstIterator& iterator) const
+        /*
+        * Not-Equal operator of Iterator class
+        *
+        * @param other - Reference to Iterator object to be compared with
+        * @ return
+        *      True if objects are different, False if equal
+        */
+        bool operator!=(const ConstIterator& iterator) const
         {
             return !(*this == iterator);
         }
 
-		/*Exception thrown when there's an attempt to affect a const iterator
-		that has reached the end of its queue
-		class InvalidOperation : public std::logic_error
-		{
-		public:
+        /*Exception thrown when there's an attempt to affect a const iterator
+        that has reached the end of its queue
+        class InvalidOperation : public std::logic_error
+        {
+        public:
 
-			* C'tor of InvalidOperation class
+            * C'tor of InvalidOperation class
 
-			InvalidOperation() :
+            InvalidOperation() :
             std::logic_error("Const-Iterator has reached the end of the queue")
             {
             }
-		};*/
+        };*/
         class InvalidOperation {};
-		friend class Queue<T>;  //IS THIS NECCESERY?
+        friend class Queue<T>;  //IS THIS NECCESERY?
 
-	private:
-		const Queue<T>* m_queue;
-		int m_index;
-	};
+    private:
+        const Queue<T>* m_queue;
+        int m_index;
+    };
 #pragma endregion
 
 #pragma region Exceptions
@@ -502,29 +502,29 @@ public:
 
 #pragma region Element
 private:
-	template <class T>
-	/*This class represents a single element of a Queue,
-	and behaves like a linked list data structure*/
-	class Element
-	{
-	public:
-		/*
-		* C'tor of Element Class
-		*/
-		Element(const T data) : m_data(data)
-		{
-			m_nextElement = NULL;
-		}
+    template <class T>
+    /*This class represents a single element of a Queue,
+    and behaves like a linked list data structure*/
+    class Element
+    {
+    public:
+        /*
+        * C'tor of Element Class
+        */
+        Element(const T data) : m_data(data)
+        {
+            m_nextElement = NULL;
+        }
 
-		/*
-		* Copy C'tor of Element class.
-		*
-		* @param source - reference to an Element object.
-		* @return
-		*      reference to an Element object.
-		*/
-		Element(const Element& source):
-        m_data(source.m_data), m_nextElement(NULL)
+        /*
+        * Copy C'tor of Element class.
+        *
+        * @param source - reference to an Element object.
+        * @return
+        *      reference to an Element object.
+        */
+        Element(const Element& source):
+                m_data(source.m_data), m_nextElement(NULL)
         {
         }
 
@@ -539,44 +539,44 @@ private:
         }
 
 
-		/*
-		* Assignment operator of Element class.
-		*
-		* @param element - reference to an Element object.
-		* @return
-		*      reference to an Element object.
-		*/
-		Element& operator=(const Element& element) = default;
+        /*
+        * Assignment operator of Element class.
+        *
+        * @param element - reference to an Element object.
+        * @return
+        *      reference to an Element object.
+        */
+        Element& operator=(const Element& element) = default;
 
-		/*
-		* D'tor of Element Class
-		*/
-		~Element() = default;
+        /*
+        * D'tor of Element Class
+        */
+        ~Element() = default;
 
 
-		Element getLast()
-		{
-			Element<T>* last = this;
-			while (last->m_nextElement) {
-				last = last->m_nextElement;
-			}
-			return *last;
-		}
+        Element getLast()
+        {
+            Element<T>* last = this;
+            while (last->m_nextElement) {
+                last = last->m_nextElement;
+            }
+            return *last;
+        }
 
-		T& getData()
-		{
-			return m_data;
-		}
+        T& getData()
+        {
+            return m_data;
+        }
 
-		Element *getNextElement()
-		{
-			return m_nextElement;
-		}
+        Element *getNextElement()
+        {
+            return m_nextElement;
+        }
 
-	private:
-		T m_data;
-		Element* m_nextElement;
-	};
+    private:
+        T m_data;
+        Element* m_nextElement;
+    };
 #pragma endregion
 #pragma endregion
 };
